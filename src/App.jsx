4ed4afb8bd63895fcb16f3eff51be1bc8d96ce12
rorodev <bodouson28@gmail.com>
2026-05-04 09:53:57 +0200

    FIX NAV PAGE

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Nav from './nav'
+import Nav from './Nav'
 import Dashboard from './pages/Dashboard.jsx'
 import Ore from './pages/Ore.jsx'
 import './App.css'
@@ -9,28 +9,28 @@ import './App.css'
 
 // function Dashboard()    { return <h2>Dashboard</h2>; }
 // function Ore()          { return <h2>Ore Lavorative</h2>; }
-function Permessi()     { return <h2>Permessi e Ferie</h2>; }
-function Report()       { return <h2>Report</h2>; }
+function Permessi() { return <h2>Permessi e Ferie</h2>; }
+function Report() { return <h2>Report</h2>; }
 
 function App() {
 
   return (
 
-       <BrowserRouter>
-        <div className="app-layout">
-          <Nav />
-          <Ore />
-          <main className="contents">
-            <Routes>
-              <Route path="/"            element={<Dashboard />} />
-              <Route path="/ore"         element={<Ore />} />
-              <Route path="/permessi"    element={<Permessi />} />
-              <Route path="/report"      element={<Report />} />
-            </Routes>
-          </main>
-        </div>
-       </BrowserRouter> 
-    
+    <BrowserRouter>
+      <div className="app-layout">
+        <Nav />
+        <Ore />
+        <main className="contents">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/ore" element={<Ore />} />
+            <Route path="/permessi" element={<Permessi />} />
+            <Route path="/report" element={<Report />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
